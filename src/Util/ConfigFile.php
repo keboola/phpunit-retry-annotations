776194e1b43946160af3ccf6a6bdcf9a1e9047c3
Roman Bracinik <roman.bracinik@keboola.com>
2024-03-05 19:20:26 +0100

    fix get root dir

--- src/Util/ConfigFile.php
+++ src/Util/ConfigFile.php
@@ -6,10 +6,14 @@ final class ConfigFile
 {
     public static function getConfigFilename(): string
     {
-        $cwd = getcwd() . \DIRECTORY_SEPARATOR;
+        $reflection = new \ReflectionClass(\Composer\Autoload\ClassLoader::class);
+        $fileName = $reflection->getFileName();
+        assert(is_string($fileName), 'ReflectionClass::getFileName() returns string');
+        $vendorDir = dirname(dirname($fileName));
 
-        if (file_exists($cwd . 'phpunit-retry.xml')) {
-            $configFilename = $cwd . 'phpunit-retry.xml';
+        $projectRoot = dirname($vendorDir);
+        if (file_exists($projectRoot . \DIRECTORY_SEPARATOR . 'phpunit-retry.xml')) {
+            $configFilename = $projectRoot . \DIRECTORY_SEPARATOR . 'phpunit-retry.xml';
         } else {
             return dirname(__FILE__) . \DIRECTORY_SEPARATOR . 'phpunit-retry.xml';
         }
